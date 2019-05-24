69da6bf900f6797ff95655f386ab3f6aa3247009
Kevin Truong <kevintru0ng@yahoo.com>
2019-05-24 15:01:21 -0400

    added channel header buttons

--- client/src/components/main-container/header/flex-header/FlexHeader.js
+++ client/src/components/main-container/header/flex-header/FlexHeader.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import FlexHeaderActions from './FlexHeaderActions/FlexHeaderActions';
+
 import '../../../../styles/FlexHeader.scss';
+import Search from '@carbon/icons-react/es/search/20';
 
 const FlexHeader = () => {
   return (
-    <div>
-
+    <div className="flex_header">
+      <div className="search_container">
+        <form className="search_form">
+          <Search className="icon_search"></Search>
+          <div className="search_form_text">Search</div>
+        </form>
+      </div>
+      <FlexHeaderActions></FlexHeaderActions>
     </div>
   );
 }
---
+++ client/src/components/main-container/header/flex-header/FlexHeaderActions/FlexHeaderActions.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../../../../../styles/FlexHeaderActions.scss';
+import At from '@carbon/icons-react/es/at/20';
+import Star from '@carbon/icons-react/es/star/20';
+import Overflow from '@carbon/icons-react/es/overflow-menu--vertical/20';
+
+const FlexHeaderActions = () => {
+  return (
+    <div className="channel_header_buttons">
+      <button className="channel_info_btns" title="Show Activity">
+        <At className="icon_at"></At>
+      </button>
+      <button className="channel_info_btns" title="Show Starred Items">
+        <Star className="flex_icon_star"></Star>
+      </button>
+      <button className="channel_info_btns" title="More Items">
+        <Overflow className="icon_overflow"></Overflow>
+      </button>
+    </div>
+  );
+}
+
+export default FlexHeaderActions;
